54c68e89d76245817080972b04aba7b689ee1300
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-04-19 12:00:45 -0300

    Adiciona link para cadastro de avaliado

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe 'Home page' do
-  it 'should display Hello world' do
+  it 'should display link to register new participant' do
     visit root_path
 
-    expect(page).to have_content 'Hello world'
+    expect(page).to have_link 'Cadastrar novo avaliado'
   end
 end
