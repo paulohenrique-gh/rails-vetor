3534fc4d301fc7c8e552c1eb36591c7d259b94d7
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-04-21 11:34:07 -0300

    Atualiza README.md
    Incluso link para documentação feita no dbdiagram.io

--- app/mailers/participant_instrument_mailer.rb
+++ app/mailers/participant_instrument_mailer.rb
@@ -2,8 +2,8 @@ class ParticipantInstrumentMailer < ApplicationMailer
   def notify_participant
     @participant = params[:participant_instrument].participant
     @instrument = params[:participant_instrument].instrument
-    @participant_instrument_id = params[:participant_instrument].id
-    @link = "http://localhost:3000/participant_instruments/#{@participant_instrument_id}/validation"
+    id = params[:participant_instrument].id
+    @link = "http://localhost:3000/participant_instruments/#{id}/validation"
 
     mail(subject: t(:mail_subject), to: @participant.email)
   end
