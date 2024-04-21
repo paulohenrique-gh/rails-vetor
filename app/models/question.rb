5b5ca26ce07be997bfc43f028a904aee59593fe6
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-04-21 16:10:22 -0300

    Adiciona testes unitários para Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :question_set
+
+  validates :description, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#valid?' do
+    it 'returns false when description is empty' do
+      participant = create(:participant)
+      instrument = create(:instrument)
+      participant_instrument = participant.participant_instruments.create!(instrument:)
+      question_set = participant_instrument.create_question_set
+      question = build(:question, description: '', question_set:)
+
+      expect(question.valid?).to be false
+      expect(question.errors).to include :description
+    end
+
+    it 'returns false when not associated with a question_set' do
+      question = build(:question, description: 'Descrição da questão', question_set: nil)
+
+      expect(question.valid?).to be false
+      expect(question.errors).to include :question_set
+    end
+
+    it 'returns true when all fields are valid' do
+      participant = create(:participant)
+      instrument = create(:instrument)
+      participant_instrument = participant.participant_instruments.create!(instrument:)
+      question_set = participant_instrument.create_question_set
+      question = build(:question, description: 'Descrição da questão', question_set:)
+
+      expect(question.valid?).to be true
+      expect(question.errors).to be_empty
+    end
+  end
 end
