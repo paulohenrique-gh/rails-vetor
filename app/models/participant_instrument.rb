3507d360ac7c4c8323de53a785bfce6fd1f54098
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-04-21 15:54:22 -0300

    Adiciona testes unitários para QuestionSet

--- app/models/participant_instrument.rb
+++ app/models/participant_instrument.rb
@@ -2,5 +2,7 @@ class ParticipantInstrument < ApplicationRecord
   belongs_to :participant
   belongs_to :instrument
 
+  has_one :question_set
+
   enum status: { pending: 0, finished: 1 }
 end
--- spec/models/question_set_spec.rb
+++ spec/models/question_set_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe QuestionSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#valid?' do
+    it 'returns false if not associated with a participant_instrument' do
+      question_set = build(:question_set, participant_instrument: nil)
+
+      expect(question_set.valid?).to be false
+      expect(question_set.errors).to include :participant_instrument
+    end
+
+    it 'returns true when associated with a participant_instrument' do
+      participant = create(:participant)
+      instrument = create(:instrument)
+      participant_instrument = participant.participant_instruments.create!(instrument:)
+      question_set = participant_instrument.build_question_set
+
+      expect(question_set.valid?).to be true
+    end
+  end
 end
