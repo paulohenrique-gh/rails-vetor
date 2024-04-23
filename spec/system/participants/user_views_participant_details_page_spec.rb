39a82c381e7dd1028841e19eaa87d49eb6c2b72e
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-04-23 15:11:04 -0300

    Remove descrição do instrumento da página de detalhes do avaliado
    Descrição já está na página de detalhes do instrumento

--- spec/system/participants/user_views_participant_details_page_spec.rb
+++ spec/system/participants/user_views_participant_details_page_spec.rb
@@ -26,30 +26,27 @@ describe 'User views participant page' do
 
     within 'table tr:first-child' do
       expect(page).to have_selector 'th:nth-of-type(1)', text: 'Nome'
-      expect(page).to have_selector 'th:nth-of-type(2)', text: 'Descrição'
-      expect(page).to have_selector 'th:nth-of-type(3)', text: 'Status'
-      expect(page).to have_selector 'th:nth-of-type(4)', text: 'Data de aplicação'
-      expect(page).to have_selector 'th:nth-of-type(5)', text: 'Data de finalização'
-      expect(page).to have_selector 'th:nth-of-type(6)', text: 'Pontuação'
+      expect(page).to have_selector 'th:nth-of-type(2)', text: 'Status'
+      expect(page).to have_selector 'th:nth-of-type(3)', text: 'Data de aplicação'
+      expect(page).to have_selector 'th:nth-of-type(4)', text: 'Data de finalização'
+      expect(page).to have_selector 'th:nth-of-type(5)', text: 'Pontuação'
     end
 
     within 'table tr:nth-child(2)' do
       expect(page).to have_selector 'td:nth-of-type(1)', text: 'Teste de Depressão'
-      expect(page).to have_selector 'td:nth-of-type(2)', text: 'Avalia probalidade de depressão'
-      expect(page).to have_selector 'td:nth-of-type(3)', text: 'Pendente'
-      expect(page).to have_selector 'td:nth-of-type(4)', text: '29/01/2022'
-      expect(page).to have_selector 'td:nth-of-type(5)', text: ''
+      expect(page).to have_selector 'td:nth-of-type(2)', text: 'Pendente'
+      expect(page).to have_selector 'td:nth-of-type(3)', text: '29/01/2022'
+      expect(page).to have_selector 'td:nth-of-type(4)', text: ''
       # TODO: update score here after implementing it in other issue
-      expect(page).to have_selector 'td:nth-of-type(6)', text: ''
+      expect(page).to have_selector 'td:nth-of-type(5)', text: ''
     end
 
     within 'table tr:nth-child(3)' do
       expect(page).to have_selector 'td:nth-of-type(1)', text: 'Teste de Ansiedade Geral'
-      expect(page).to have_selector 'td:nth-of-type(2)', text: 'Avalia possibilidade de quadro ansioso'
-      expect(page).to have_selector 'td:nth-of-type(3)', text: 'Finalizado'
-      expect(page).to have_selector 'td:nth-of-type(4)', text: '14/03/2021'
-      expect(page).to have_selector 'td:nth-of-type(5)', text: '17/03/2021'
-      expect(page).to have_selector 'td:nth-of-type(6)', text: ''
+      expect(page).to have_selector 'td:nth-of-type(2)', text: 'Finalizado'
+      expect(page).to have_selector 'td:nth-of-type(3)', text: '14/03/2021'
+      expect(page).to have_selector 'td:nth-of-type(4)', text: '17/03/2021'
+      expect(page).to have_selector 'td:nth-of-type(5)', text: ''
     end
   end
 
